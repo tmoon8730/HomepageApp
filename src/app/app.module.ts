55659bce829923f5c12dc0eaf3fca7b313f715e6
Tyler Moon <moon.tyler@gmail.com>
2017-10-06 17:49:49 -0400

    Added app-link-grid component for the links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// MatGrid stuff
+import { MatGridListModule } from '@angular/material';
+
 import { AppComponent } from './app.component';
+import { LinkGridComponent } from './link-grid/link-grid.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LinkGridComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/link-grid/link-grid.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-link-grid',
+  templateUrl: './link-grid.component.html',
+  styleUrls: ['./link-grid.component.css']
+})
+export class LinkGridComponent {
+
+  // Grid configuration
+  gridConfig = {numOfCols: 2, rowHeight: '100px'};
+  // Definition for each tile //TODO: Replace this data with a call to a MongoDB
+  tiles = [
+    {text: 'Google', cols: 1, rows: 1, color: 'lightyellow', link: 'https://www.google.com'},
+    {text: 'Best Subreddit', cols: 1, rows: 1, color: 'lightgreen', link: 'https://reddit.com/r/me_irl'},
+    {text: 'Ye ol Chan', cols: 1, rows: 1, color: 'lightpink', link: 'https://boards.4chan.org/g/'},
+    {text: 'Communication', cols: 1, rows: 1, color: '#DDBDF1', link: 'https://hangout.google.com'},
+  ];
+
+}
